0a2ff9449950072d87cac245a8b4d61a3dcedc28
CheaKimsan <cheakimsan456@gmail.com>
2026-07-14 16:44:47 +0700

    first commit

---
+++ datatype/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variable of type uint8
+	var a uint8
+	a = 10
+	fmt.Println(a)
+
+	// variable of type in t8
+	var b int = 200000
+	fmt.Println(b)
+
+	// Byte
+	var myByte byte = 'A'
+	fmt.Println(myByte)
+
+	// Rune type
+	var myRune rune = 'B'
+	fmt.Println(myRune)
+
+	var name string = "Chea Kimsan"
+	var year int = 3
+	var cause string = "MIS"
+	var dob string = "2003-09-07"
+
+	fmt.Println(name)
+	fmt.Println(year)
+	fmt.Println(cause)
+	fmt.Println(dob)
+
+}
---
+++ variable/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string no vlaue is = ""
+	var myString string
+	fmt.Println(myString)
+
+	myString = "Welcome to GDI"
+	fmt.Println(myString)
+
+	// int no vlaue is = 0
+	var myInt int
+	fmt.Println(myInt)
+
+	myInt = 10
+	fmt.Println(myInt)
+
+	// boolean default value is false
+	var myBoolean bool
+	fmt.Println(myBoolean)
+
+	myBoolean = true
+	fmt.Println(myBoolean)
+
+}
