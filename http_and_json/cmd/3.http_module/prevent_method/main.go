74f062c6ccb75fefbf261951b7e717df78adf4a8
CheaKimsan <cheakimsan456@gmail.com>
2026-07-17 17:04:18 +0700

    fix: update helloHandler to return early on non-GET requests and correct port number in main

--- http_and_json/cmd/3.http_module/prevent_method/main.go
+++ http_and_json/cmd/3.http_module/prevent_method/main.go
@@ -8,14 +8,14 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Only Get Method is Allowed", http.StatusMethodNotAllowed)
+		return
 	}
+
+	_, _ = w.Write([]byte("Hello from go net/http server"))
 }
 func main() {
 	http.HandleFunc("/hello", helloHandler)
-
-	fmt.Println("Try going to 8080 port")
-
-	err := http.ListenAndServe(":8080", nil)
-
+	fmt.Println("Try going to 5000 port")
+	err := http.ListenAndServe(":5000", nil)
 	fmt.Println(err)
 }
